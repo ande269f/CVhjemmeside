3a681a5d99a7ad9a96af1c9f557089609d1a714a
ande269f <andersknudsen98@gmail.com>
2025-07-25 20:25:42 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 style={{textAlign: 'center'}}>Hello world</h1>
+        </div>
+    );
+}
+
+export default App;
